6bac05e4a37dba59a381ed75acc1ce6cbe92c53b
Jonas Dos <jonas.dsds@hotmail.com>
2024-12-03 23:45:14 -0300

    Feat. Conexão com banco de dados e Models Prisma criados.

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,14 @@
-import express, { Request, Response } from "express"
-import dotenv from "dotenv"
+import express, { Request, Response } from "express" 
+import dotenv from "dotenv" 
 
-dotenv.config()
+dotenv.config() 
 const app = express()
 app.use(express.json())
 
 app.get("/health", (req: Request, res: Response) => {
-  res.sendStatus(200)
+  res.status(200).send("I'm, OK!")
 })
 
-
-app.listen(process.env.PORT || 5500, ()=> {
+app.listen(process.env.PORT, ()=> {
   console.log(`Servidor rodando na porta: ${process.env.PORT}`)
 })
