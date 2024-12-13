46daf9652ea73c5d2cc37e581d6445f9a96f2067
Jonas Dos <jonas.dsds@hotmail.com>
2024-12-13 16:46:07 -0300

    incluindo a pasta dist no gitignore

--- dist/src/routes/erase.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var erase_1 = require("../controllers/erase");
-var express_1 = require("express");
-var jwtvalidation_1 = require("../middlewares/jwtvalidation");
-var eraseRouter = (0, express_1.Router)();
-eraseRouter.delete("/erase", jwtvalidation_1.jwtvalidation, erase_1.eraseAcount);
-exports.default = eraseRouter;
